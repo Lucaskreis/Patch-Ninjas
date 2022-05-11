41732b72ea7d07e4fc24a515f670b417cc5c5b35
Lucas Reis <lucaskreis.com>
2022-05-11 16:06:00 -0300

    first commit

---
+++ models/Jobs.model.js
@@ -0,0 +1,16 @@
+const { Schema, model, default: mongoose } = require("mongoose");
+
+const jobsSchema = new Schema({
+    user: {type: mongoose.Types.ObjectId, ref: "User"},
+    title: {type: String, required: true, maxlength: 25, minlength: 10},
+    description: {type: String, required: true, maxlength: 200, minlength: 25},
+    tags: {type: String, required: true},
+    prazo: {type: String, required: true},
+    data: {type: Date, default: Date.now},
+    local: {type: String, required: true}
+
+});
+
+const JobsModel = model("Jobs", jobsSchema);
+
+module.exports = JobsModel
