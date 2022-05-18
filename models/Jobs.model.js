02082944817ec00a76f2cdfe2579b77d5b862297
dante.tatsch@hotmail.com <dante.tatsch@gmail.com>
2022-05-18 11:25:02 +0100

    tags

--- models/Jobs.model.js
+++ models/Jobs.model.js
@@ -4,7 +4,7 @@ const jobsSchema = new Schema({
     user: {type: mongoose.Types.ObjectId, ref: "User"},
     title: {type: String, required: true, maxlength: 25, minlength: 10},
     description: {type: String, required: true, maxlength: 200, minlength: 15},
-    tags: {type: String, required: true},
+    tags: {type: String, required: true, enum: ["WD", "UXUI","DI"],},
     prazo: {type: String, required: true},
     data: {type: Date, default: Date.now},
     local: {type: String, required: true},
