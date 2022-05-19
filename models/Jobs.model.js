590e56836c2509b7dcc7faa3ebbaaa1462c5cc5d
Lucas Reis <lucaskreis.com>
2022-05-19 10:40:45 -0300

    ajustes antes do deploy

--- models/Jobs.model.js
+++ models/Jobs.model.js
@@ -2,9 +2,9 @@ const { Schema, model, default: mongoose } = require("mongoose");
 
 const jobsSchema = new Schema({
     user: {type: mongoose.Types.ObjectId, ref: "User"},
-    title: {type: String, required: true, maxlength: 25, minlength: 10},
-    description: {type: String, required: true, maxlength: 200, minlength: 15},
-    tags: {type: String, required: true},
+    title: {type: String, required: true, minlength: 1},
+    description: {type: String, required: true, maxlength: 200, minlength: 5},
+    tags: {type: String, required: true, enum:["WD", "UXUI", "DI"]},
     prazo: {type: String, required: true},
     data: {type: Date, default: Date.now},
     local: {type: String, required: true},
